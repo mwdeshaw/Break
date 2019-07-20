a9548dce78575bbe62a9f97dba8ac9261e58dc55
mwdeshaw <mwdeshaw@gmail.com>
2019-07-20 16:52:28 -0700

    Add turning on sound to the gameboy

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 import Game from "./classes/game";
 import GView from "./classes/g_view";
 
-
 document.addEventListener("DOMContentLoaded", () => {
     const startBtn = document.getElementById("start-game");
     startBtn.onclick = () => {
         const light = document.getElementById('power-lt');
         light.setAttribute("class", "active");
-        // light.style.backgroundColor = "#c1e863";
+
+        const audio = document.querySelector(`audio`);
+        audio.currentTime = 0;
+        audio.play();
+
         const canvas = document.getElementById("board");
         const ctx = canvas.getContext("2d");
         const game = new Game(ctx);
         new GView(game).start();
     };
 });
-    
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-//     const canvas = document.getElementById("board");
-//     const ctx = canvas.getContext("2d");
-//     const game = new Game(ctx);
-//     new GView(game).start();
-// });
