770fed60c8bfba2667d4de5af54634bf6b6067a3
mwdeshaw <mwdeshaw@gmail.com>
2019-07-20 19:44:34 -0700

    Create moving screen and sounds

--- src/index.js
+++ src/index.js
@@ -2,25 +2,43 @@ import Game from "./classes/game";
 import GView from "./classes/g_view";
 
 document.addEventListener("DOMContentLoaded", () => {
+    const defScreen = document.getElementById("default");
     const powerBtn = document.getElementById("power-btn");
     powerBtn.onclick = () => {
-        const light = document.getElementById('power-lt');
-        light.setAttribute("class", "active");
+        defScreen.setAttribute("class", "active");
         const startScreen = document.getElementById('start-screen');
         startScreen.setAttribute("class", "active");
 
-        const audio = document.querySelector(`audio`);
-        audio.currentTime = 0;
-        audio.play();
+        const light = document.getElementById('power-lt');
+        light.setAttribute("class", "active");
 
+        const screenText = document.querySelector(".start-text");
+        screenText.classList.add("end");
         const startBtn = document.getElementById("start-game");
+        setTimeout(() => {
+            const audio = document.querySelector(`audio`);
+            audio.currentTime = 0;
+            audio.play();
+            startBtn.setAttribute("class", "active");
+        }, 3000);
+
         startBtn.onclick = () => {
             startScreen.removeAttribute("class");
+            screenText.classList.remove("end");
             const canvas = document.getElementById("board");
-            // canvas.setAttribute("class", "active");
             const ctx = canvas.getContext("2d");
             const game = new Game(ctx);
             new GView(game).start();
+        };
+
+        if (defScreen.classList[0] === "active") {
+            powerBtn.onclick = () => {
+                defScreen.removeAttribute("class");
+                startBtn.removeAttribute("class");
+                startScreen.removeAttribute("class");
+                screenText.classList.remove("end");
+                light.removeAttribute("class");
+            };
         };
     };
 });
