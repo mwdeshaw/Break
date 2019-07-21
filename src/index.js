e08787d1566bf1d606bd28dd45ffdcc569f1d75d
mwdeshaw <mwdeshaw@gmail.com>
2019-07-20 21:16:37 -0700

    Finish starting screen

--- src/index.js
+++ src/index.js
@@ -14,20 +14,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const screenText = document.querySelector(".start-text");
         const instructions = document.querySelector(".instructions");
+        const controlsList = document.querySelector(".controls-list");
 
         screenText.classList.add("end");
         const startBtn = document.getElementById("start-game");
         setTimeout(() => {
-            instructions.classList.add("end");
             const audio = document.querySelector(`audio`);
             audio.currentTime = 0;
             audio.play();
+            instructions.classList.add("end");
+            controlsList.classList.add("end");
             startBtn.setAttribute("class", "active");
         }, 3000);
 
         startBtn.onclick = () => {
             startScreen.removeAttribute("class");
             instructions.classList.remove("end");
+            controlsList.classList.remove("end");
             screenText.classList.remove("end");
             const canvas = document.getElementById("board");
             const ctx = canvas.getContext("2d");
@@ -41,6 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 startBtn.removeAttribute("class");
                 startScreen.removeAttribute("class");
                 screenText.classList.remove("end");
+                controlsList.classList.remove("end");
                 light.removeAttribute("class");
             };
         };
